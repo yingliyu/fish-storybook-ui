025c6119f8d713c68dfeb70711caca0e34fd10ac
ylyu <ylyu@sstir.cn>
2021-08-16 17:22:30 +0800

    modify config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,24 @@
 const path = require('path')
 module.exports = {
-  stories: ['../src/stories/**/*.stories.mdx', '../src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  stories: [
+    '../src/Intro.stories.mdx',
+    '../src/stories/**/*.stories.mdx',
+    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', 'less-loader'],
+      include: path.resolve(__dirname, '../')
+    })
+
+    // Return the altered config
+    return config
+  }
 }
