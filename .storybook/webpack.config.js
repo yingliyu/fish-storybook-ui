5a7c0b1cf8bd9680e7f3f620c3744d3d965c86e2
ylyu <ylyu@sstir.cn>
2021-08-16 17:44:24 +0800

    modify config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,12 @@
-// import { configure } from "@storybook/vue";
-
-// configure(require.context("../src", true, /\.stories\.js$/), module);
+// import { addParameters, configure } from '@storybook/vue'
+// function loadStories() {
+//   /*根据特定的文件名加载stories，在这个例子，用xxx.stories.js命名文件*/
+//   const req = require.context('../src', true, /\.stories\.js$/)
+//   req.keys().forEach((filename) => req(filename))
+// }
+// addParameters({
+//   options: {
+//     panelPosition: 'right' //操作面板在右边
+//   }
+// })
+// configure(loadStories, module)
